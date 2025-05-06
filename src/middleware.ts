70a0a485463ab5aa81af15ea201ccf7a70167a34
yxsifdev <yosifblancotafur2006@gmail.com>
2025-05-06 10:21:15 -0500

    Eliminar el middleware onRequest

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,10 @@
 import type { MiddlewareHandler } from 'astro';
 
-export const onRequest: MiddlewareHandler = async (context, next) => {
+export function onRequest: MiddlewareHandler (context, next) {
+  // interceptar los datos de una solicitud.
+  // opcionalmente, modifica las propiedades en `locals`.
+  context.locals.title = "Nuevo título";
+
+  // devuelve una respuesta o el resultado de llamar a `next()`.
   return next();
 };
