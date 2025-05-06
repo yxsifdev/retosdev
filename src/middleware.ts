5d3b1771188ee6f2e4e58ca799622b4ad85740b4
yxsifdev <yosifblancotafur2006@gmail.com>
2025-05-06 10:14:52 -0500

    Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,5 @@
-// import { defineMiddleware } from 'astro:middleware';
-// import { getSession } from 'auth-astro/server';
+import type { MiddlewareHandler } from 'astro';
 
-// export const onRequest = defineMiddleware(async (context, next) => {
-//   const { url, request, redirect } = context;
-//   const pathname = new URL(url).pathname;
-
-//   // Verificar rutas protegidas
-//   const isProtectedRoute = pathname.startsWith('/learn') || pathname.startsWith('/api');
-
-//   // Si es una ruta protegida
-//   if (isProtectedRoute) {
-//     const session = await getSession(request); // Implementa esta función según tu auth
-
-//     if (!session) {
-//       if (pathname.startsWith('/api')) {
-//         return redirect('/', 301);
-//       } else {
-//         return redirect('/', 302);
-//       }
-//     }
-
-//     // Si está autenticado pero accede directamente a /api
-//     if (pathname.startsWith('/api') && request.headers.get('sec-fetch-dest') !== 'fetch') {
-//       return redirect('/learn', 302);
-//     }
-//   }
-
-//   return next();
-// });
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  return next();
+};
